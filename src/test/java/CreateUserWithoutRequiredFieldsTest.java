2bf51760918a758ab3f809731f21e96a5cba59b7
Alfiya Khuziakhmetova <A.Khuziakhmetova@yandex.ru>
2022-07-26 21:07:31 +0300

    Diplom_2 changes

--- src/test/java/CreateUserWithoutRequiredFieldsTest.java
+++ src/test/java/CreateUserWithoutRequiredFieldsTest.java
@@ -5,7 +5,6 @@ import model.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.apache.http.HttpStatus.*;
 import static org.junit.Assert.*;
 
@@ -33,12 +32,12 @@ public class CreateUserWithoutRequiredFieldsTest {
         user.setEmail(null);
         ValidatableResponse createResponse = userClient.createUser(user);
 
+        token = userClient.getUserToken(createResponse);
+        assertNull(token);
         int statusCode = createResponse.extract().statusCode();
         assertEquals("Код статуса отличается от ожидаемого результата", SC_FORBIDDEN, statusCode);
         boolean result = createResponse.extract().path("success");
         assertFalse(result);
-        token = userClient.getUserToken(createResponse);
-        assertNull(token);
     }
 
     @Test
@@ -47,12 +46,12 @@ public class CreateUserWithoutRequiredFieldsTest {
         user.setPassword(null);
         ValidatableResponse createResponse = userClient.createUser(user);
 
+        token = userClient.getUserToken(createResponse);
+        assertNull(token);
         int statusCode = createResponse.extract().statusCode();
         assertEquals("Код статуса отличается от ожидаемого результата", SC_FORBIDDEN, statusCode);
         boolean result = createResponse.extract().path("success");
         assertFalse(result);
-        token = userClient.getUserToken(createResponse);
-        assertNull(token);
     }
 
     @Test
@@ -61,11 +60,11 @@ public class CreateUserWithoutRequiredFieldsTest {
         user.setName(null);
         ValidatableResponse createResponse = userClient.createUser(user);
 
+        token = userClient.getUserToken(createResponse);
+        assertNull(token);
         int statusCode = createResponse.extract().statusCode();
         assertEquals("Код статуса отличается от ожидаемого результата", SC_FORBIDDEN, statusCode);
         boolean result = createResponse.extract().path("success");
         assertFalse(result);
-        token = userClient.getUserToken(createResponse);
-        assertNull(token);
     }
 }
